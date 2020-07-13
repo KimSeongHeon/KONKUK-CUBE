3bce5c2a6dffc678cd94bcdd7552b16772f435fa
KimSeongHeon <rlat302@naver.com>
2020-07-14 00:31:00 +0900

    2020.07.14
    1. 끝~

--- app/src/main/java/com/project/reservation_kcube/NoticeDialog.kt
+++ app/src/main/java/com/project/reservation_kcube/NoticeDialog.kt
@@ -2,32 +2,38 @@ package com.project.reservation_kcube
 
 import android.app.Dialog
 import android.content.Context
+import android.os.Environment
+import android.util.Log
 import android.view.Window
+import android.widget.Button
 import android.widget.TextView
 import java.io.BufferedReader
-import java.io.FileInputStream
 import java.io.IOException
 import java.io.InputStreamReader
 
 
 class NoticeDialog(var context: Context?) {
+    val filePath = Environment.getExternalStorageDirectory().getAbsolutePath()
     fun callFunction() {
         val dlg = Dialog(context)
         dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dlg.setContentView(R.layout.notice_dialog)
         dlg.show()
         var notice = dlg.findViewById<TextView>(R.id.notice)
+        var okbutton = dlg.findViewById<Button>(R.id.notice_okButton)
         notice.text = loadText()
+        okbutton.setOnClickListener {
+            dlg.dismiss()
+        }
     }
     fun loadText():String{
         val strBuffer = StringBuffer()
         try {
-            val inputstream = FileInputStream("notice.txt")
+            val inputstream = context!!.resources.openRawResource(R.raw.notice)
             val reader = BufferedReader(InputStreamReader(inputstream))
-            var line = ""
-            while (reader.readLine() != null) {
-                line = reader.readLine()
-                strBuffer.append(line + "\n")
+            for (item in reader.readLines()){
+                strBuffer.append(item)
+                strBuffer.append("\n\n")
             }
             reader.close()
             inputstream.close()
@@ -35,7 +41,7 @@ class NoticeDialog(var context: Context?) {
             e.printStackTrace()
             return ""
         }
-
+        Log.v("str",strBuffer.toString())
         return strBuffer.toString()
     }
 }
